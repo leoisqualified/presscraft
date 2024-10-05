df0f7f0e6d3d64396673ebf0aedf02890d8044e3
andy <eaadenutsi@st.ug.edu.gh>
2024-10-05 12:22:18 +0000

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
 import Navbar from './components/Navbar.jsx';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/Home';
+import Services from './components/Services';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-    <Navbar />
-
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/aboutus' element={<AboutUs />} />
+          <Route path='/contactus' element={<ContactUs />} />
+        </Routes>
     </Router>
   )  
 }
